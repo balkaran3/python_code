fcde435aa204d15c2340e0ce9db136ed2f5e6d91
Balkara sidhu <balkarnsidhu33@gmail.com>
2024-07-04 13:06:57 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,26 @@
+# print('hello world')
+# num1=[200.25,'gcfzerd']
+# print(num1)
+# print(type(num1))
+
+# var1=True
+# var2=False
+# print(type(var1))
+# print(type(var2))
+
+st='upflairs up'
+# print(st)
+# print(len(st))
+# print(st[2])
+# print(st[::2])\
+# print(st.upper())
+# print(st.lower())
+# print(st.capitalize())     #for 1st letter capital
+# print(st.title())         # for all words capitalize 1st letter
+
+# print(st.isupper())          #for questioning
+# print(st.find('u',2))
+
+
+
+
---
+++ day2.py
@@ -0,0 +1,85 @@
+#string
+# st =                #variable declare in string only a-z,A-Z,0-9,_
+
+# age=10
+# Age=23          #these are three different variables
+# AGE=88
+
+# string data types
+# st= 'upflairs'
+# print(st)
+# print(type(st))
+# print(st[0])
+# print(st[4:7])
+
+# st='upflairs pvt ltd jaipur rajasthan'
+# print(st2[17:-10])     #[start : ending : jump]    
+# print(st2[17:-10])     #[start : ending : jump]    
+# print(len(st2))
+# # print(st2[:8])
+# print(st2[:])
+# print(st2[::-1])
+# print(st.upper())
+# print(st.lower())
+# print(st.title())
+# print(st.capitalize())
+# print(st.index('i',20))
+# print(st.replace('upflairs','tfysr'))
+
+# list
+# ls=[22,332,3121.31,'dxsde','ddq21ww2',True,500]
+# print(type(ls))
+
+# student_name=['mohit','rohit','kanak','sharma']
+# print(student_name[-1])
+
+# from ast import Del
+
+
+# ls=['a','b','c','d','e','f','g']
+# print(ls[-5:-2])
+# print(ls[5:])
+# print(ls[-2:])
+# ls[3]='don'
+# print(ls)
+
+# ls.append('i')
+# print(ls)
+
+# ls.pop()
+# print(ls)
+
+# ls.insert(2,'upflair')
+# print(ls)
+
+# ls.remove('e')
+# print(ls)
+# del ls[2]
+# print(ls)
+
+# ls1=['a','b','c','d','e','f','g','h']
+# ls2=['f','g','h']
+
+# # ls1.append(ls2)  
+# # print(ls1)
+# ls1.extend(ls2)
+# print(ls1)
+
+  ### reverse,sort,copy,clear
+# ls1.reverse()
+# print(ls1)
+# print(ls1.sort())
+
+# print(ls1.copy())
+# ls1.clear()
+# print(ls1)
+
+# ls=[16,11,34,23,33,4]
+# ls2=ls
+# ls2[2]='upflairs'
+# print(ls)
+# print(ls2)
+
+# ls2=ls.copy()
+# ls2[2]='upflairs'
+# print(ls2)
